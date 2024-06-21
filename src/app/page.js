1a7bed14bcb3d96a124d736566c62b2c782dd233
Alejandra <ale.szuster@gmail.com>
2024-06-20 22:03:57 -0300

    transicion inicio

--- src/app/page.js
+++ src/app/page.js
@@ -17,9 +17,9 @@ export default function Home() {
           alt=""
           className="absolute top-[50px] right-0 w-[182px] lg:w-[311px]"
         />
-      <h2 className="text-mudGreenDark tracking-[0.2em] font-semibold text-[17px] lg:text-[22px] leading-[17px] lg:leading-[22px] mb-[54px]">23 . 11 . 2024</h2>
-      <h1 className="font-extralight text-mudGreenDark tracking-[0.15em] lg:tracking-[0.2em] text-[34px] leading-[34px] lg:leading-[90px] lg:text-[90px]">Jose y Mariano</h1>
-      <h3 className="font-rbeanie text-mudGreenDark text-[30px] lg:text-[78px] mt-[15px]">Mendoza, Argentina</h3>
+      <h2 className="text-mudGreenDark tracking-[0.2em] font-semibold text-[17px] lg:text-[22px] leading-[17px] lg:leading-[22px] mb-[54px] animate-slidein300 opacity-0">23 . 11 . 2024</h2>
+      <h1 className="font-extralight text-mudGreenDark tracking-[0.15em] lg:tracking-[0.2em] text-[34px] leading-[34px] lg:leading-[90px] lg:text-[90px] animate-slidein500 opacity-0">Jose y Mariano</h1>
+      <h3 className="font-rbeanie text-mudGreenDark text-[30px] lg:text-[78px] mt-[15px] animate-slidein700 opacity-0">Mendoza, Argentina</h3>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,23 @@ module.exports = {
       '3xl': '1920px'
     },
     extend: {
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein300: "slidein 1s ease 300ms forwards",
+        slidein500: "slidein 1s ease 500ms forwards",
+        slidein700: "slidein 1s ease 700ms forwards",
+      },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
         rbeanie: ["Reenie Beanie", "sans-serif"],
